42c86276c57d88b080c7bf6d91d4bca905b442e9
Jeff Maley <jeffmaley@gmail.com>
2022-01-14 22:14:36 -0800

    Working with comments and pylint compliance

--- README.md
+++ README.md
@@ -7,9 +7,14 @@ Stop running AWS resources  after a budget limit is reached.
 This application performs the following actions when triggered:
 
 * Stop all running EC2 instances in all regions
-* Remove all Auto-Scaling groups
+* Remove all Auto-Scaling groups from all regions
 * Block all public access to S3
-* Disable all IAM Users
+* Disable all IAM Users' access keys and passwords
+
+## Installation
+
+* Upload the awsstopall-lambda.zip to an S3 bucket
+* Run the cloudformation template `awsstopall-cloudformation.yaml` and provide the necessary values.
 
 ## TODO
 
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import boto3
+"""This script disables IAM user passwords and access keys,
+stops running ec2 instances, deletes autoscaling groups, and
+adds public blocks to s3 buckets"""
+
+import os
 import sys
 import logging
 import traceback
 import json
-import os
+import boto3
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,9 +19,10 @@ block_s3 = os.environ['BLOCK_S3']
 delete_autoscaling_groups = os.environ['DELETE_AUTOSCALING_GROUPS']
 
 
-def get_regions(ec2):
+def get_regions():
     """Return all active regions"""
-    result = ec2.describe_regions()
+    ec2_base = boto3.client('ec2')
+    result = ec2_base.describe_regions()
     return result['Regions']
 
 
@@ -51,7 +57,7 @@ def get_ec2_instances_helper(ec2, next_token):
 
 
 def get_ec2_instances(ec2):
-    """Parent function for getting running EC2 instances."""
+    """Get running EC2 instances."""
     running_instances = []
     result = get_ec2_instances_helper(ec2, '')
     try:
@@ -72,7 +78,7 @@ def get_ec2_instances(ec2):
     except KeyError:
         # No NextToken found so no more records.
         pass
-    except Exception as e:
+    except Exception as excp: # pylint: disable=broad-except
         exception_type, exception_value, exception_traceback = sys.exc_info()
         traceback_string = traceback.format_exception(
                                         exception_type,
@@ -85,36 +91,66 @@ def get_ec2_instances(ec2):
             "stackTrace": traceback_string
         })
         logger.error(err_msg)
-        logger.error(e)
+        logger.error(excp)
     return running_instances
 
 
-def stop_ec2_instances(ec2, instance_id):
+def stop_running_ec2_instances(ec2, instance_id):
     """Stop EC2 instance"""
-    result = ec2.stop_instances(
-                InstanceIds=[instance_id]
-    )
-    logger.info(result)
+    try:
+        logger.info("Stopping instance %s", instance_id)
+        ec2.stop_instances(
+                    InstanceIds=[instance_id]
+        )
+    except Exception as excp: # pylint: disable=broad-except
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(
+                                        exception_type,
+                                        exception_value,
+                                        exception_traceback
+                                        )
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
+        logger.error(excp)
 
 
-def get_s3_buckets(s3):
+def get_s3_buckets(s3_client):
     """Return list of S3 buckets"""
-    result = s3.list_buckets()
+    result = s3_client.list_buckets()
     return result['Buckets']
 
 
-def block_s3_public_access(s3, bucket):
+def block_s3_public_access(s3_client, bucket):
     """Apply Public Access Block on an S3 bucket"""
-    result = s3.put_public_access_block(
-                Bucket=bucket,
-                PublicAccessBlockConfiguration={
-                    'BlockPublicAcls': True,
-                    'IgnorePublicAcls': True,
-                    'BlockPublicPolicy': True,
-                    'RestrictPublicBuckets': True
-                }
-    )
-    logger.info(result)
+    try:
+        logger.info("Setting Block Public Access on %s", bucket)
+        s3_client.put_public_access_block(
+                    Bucket=bucket,
+                    PublicAccessBlockConfiguration={
+                        'BlockPublicAcls': True,
+                        'IgnorePublicAcls': True,
+                        'BlockPublicPolicy': True,
+                        'RestrictPublicBuckets': True
+                    }
+        )
+    except Exception as excp: # pylint: disable=broad-except
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(
+                                        exception_type,
+                                        exception_value,
+                                        exception_traceback
+                                        )
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
+        logger.error(excp)
 
 
 def get_autoscaling_groups_helper(autoscaling, next_token):
@@ -129,7 +165,7 @@ def get_autoscaling_groups_helper(autoscaling, next_token):
 
 
 def get_autoscaling_groups(autoscaling):
-    """Parent for getting AutoScaling Groups"""
+    """Get AutoScaling Groups"""
     autoscaling_groups = []
     result = get_autoscaling_groups_helper(autoscaling, '')
     autoscaling_groups.append(result['AutoScalingGroups'])
@@ -138,14 +174,14 @@ def get_autoscaling_groups(autoscaling):
         if result['NextToken']:
             old_token = 'old_token'
             while next_token != old_token:
-                result = get_autoscaling_groups(autoscaling, next_token)
+                result = get_autoscaling_groups_helper(autoscaling, next_token)
                 old_token = next_token
                 next_token = result['NextToken']
                 autoscaling_groups.append(result['AutoScalingGroups'])
     except KeyError:
         # No NextToken found so no more records.
         pass
-    except Exception as e:
+    except Exception as excp: # pylint: disable=broad-except
         exception_type, exception_value, exception_traceback = sys.exc_info()
         traceback_string = traceback.format_exception(
                                         exception_type,
@@ -158,17 +194,32 @@ def get_autoscaling_groups(autoscaling):
             "stackTrace": traceback_string
         })
         logger.error(err_msg)
-        logger.error(e)
+        logger.error(excp)
     return autoscaling_groups
 
 
 def delete_autoscaling_group(autoscaling, autoscaling_group):
     """Delete AutoScaling Group"""
-    result = autoscaling.delete_auto_scaling_group(
-                        AutoScalingGroupName=autoscaling_group,
-                        ForceDelete=True
-    )
-    logger.info(result)
+    try:
+        logger.info("Deleting AutoScalingGroup %s", autoscaling_group)
+        autoscaling.delete_auto_scaling_group(
+                            AutoScalingGroupName=autoscaling_group,
+                            ForceDelete=True
+        )
+    except Exception as excp: # pylint: disable=broad-except
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(
+                                        exception_type,
+                                        exception_value,
+                                        exception_traceback
+                                        )
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
+        logger.error(excp)
 
 
 def get_iam_users_helper(iam, marker):
@@ -197,7 +248,7 @@ def get_iam_users(iam):
     except KeyError:
         # No Marker found so no more records.
         pass
-    except Exception as e:
+    except Exception as excp: # pylint: disable=broad-except
         exception_type, exception_value, exception_traceback = sys.exc_info()
         traceback_string = traceback.format_exception(
                                         exception_type,
@@ -210,22 +261,34 @@ def get_iam_users(iam):
             "stackTrace": traceback_string
         })
         logger.error(err_msg)
-        logger.error(e)
+        logger.error(excp)
     return iam_users
 
 
 def delete_iam_user_profile(iam, iam_user):
     """Delete the password for an IAM user"""
-    result = ''
     try:
-        result = iam.delete_login_profile(
+        logger.info("Disabling password for user %s", iam_user)
+        iam.delete_login_profile(
                     UserName=iam_user
         )
     except iam.exceptions.NoSuchEntityException:
         # No LoginProfile found so no password to disable
-        result = f'No LoginProfile found for {iam_user}'
-    logger.info(result)
-    return
+        logger.info("No LoginProfile found for %s", iam_user)
+    except Exception as excp: # pylint: disable=broad-except
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(
+                                        exception_type,
+                                        exception_value,
+                                        exception_traceback
+                                        )
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
+        logger.error(excp)
 
 
 def disable_iam_user_access_keys(iam_resource, iam_user):
@@ -233,66 +296,79 @@ def disable_iam_user_access_keys(iam_resource, iam_user):
     iam_user_obj = iam_resource.User(iam_user)
     access_keys = iam_user_obj.access_keys.all()
     for access_key in access_keys:
-        print(access_key)
+        logger.info("Disabling access key %s for user %s", access_key, iam_user)
         access_key.deactivate()
-    return
 
+
+def process_ec2(region):
+    """Parent function for processing EC2"""
+    if stop_ec2_instances == 'true':
+        ec2_client = boto3.client('ec2', region_name=region['RegionName'])
+        running_instances = get_ec2_instances(ec2_client)
+        if running_instances:
+            for instance in running_instances[0]:
+                print(instance['InstanceId'])
+                stop_running_ec2_instances(ec2_client, instance['InstanceId'])
+
+
+def process_autoscaling_groups(region):
+    """Parent function for processing AutoScalingGroups"""
+    if delete_autoscaling_groups == 'true':
+        autoscaling = boto3.client(
+                            'autoscaling',
+                            region_name=region['RegionName']
+                            )
+        autoscaling_groups = get_autoscaling_groups(autoscaling)
+        if autoscaling_groups:
+            for autoscaling_group in autoscaling_groups[0]:
+                print(autoscaling_group['AutoScalingGroupName'])
+                delete_autoscaling_group(
+                    autoscaling,
+                    autoscaling_group['AutoScalingGroupName']
+                    )
+
+def process_s3():
+    """Parent function for processing S3"""
+    s3_client = boto3.client('s3')
+    if block_s3 == 'true':
+        s3_buckets = get_s3_buckets(s3_client)
+        if s3_buckets:
+            for bucket in s3_buckets:
+                print(bucket['Name'])
+                block_s3_public_access(s3_client, bucket['Name'])
+
+
+def process_iam():
+    """Parent function for processing IAM"""
+    iam_client = boto3.client('iam')
+    iam_resource = boto3.resource('iam')
+    if disable_iam_users == 'true':
+        iam_users = get_iam_users(iam_client)
+        if iam_users:
+            for iam_user in iam_users[0]:
+                print(iam_user['UserName'])
+                delete_iam_user_profile(iam_client, iam_user['UserName'])
+                disable_iam_user_access_keys(
+                    iam_resource,
+                    iam_user['UserName']
+                    )
 
 def main(event, context):
     """Main entry point
     Process EC2 instances, AutoScaling Groups, S3 buckets, and IAM Users
     """
-    ec2_base = boto3.client('ec2')
-    s3 = boto3.client('s3')
-    iam = boto3.client('iam')
-    iam_resource = boto3.resource('iam')
-
-    regions = get_regions(ec2_base)
+    regions = get_regions()
     for region in regions:
-        print(region['RegionName'])
-        if stop_ec2_instances == 'true':
-            ec2 = boto3.client('ec2', region_name=region['RegionName'])
-            running_instances = get_ec2_instances(ec2)
-            if running_instances:
-                for instance in running_instances[0]:
-                    print(instance['InstanceId'])
-                    stop_ec2_instances(ec2, instance['InstanceId'])
+        logger.info("Processing region %s", region['RegionName'])
+        process_ec2(region['RegionName'])
+        process_autoscaling_groups(region['RegionName'])
 
-        if delete_autoscaling_groups == 'true':
-            autoscaling = boto3.client(
-                                'autoscaling',
-                                region_name=region['RegionName']
-                                )
-            autoscaling_groups = get_autoscaling_groups(autoscaling)
-            if autoscaling_groups:
-                for autoscaling_group in autoscaling_groups[0]:
-                    print(autoscaling_group['AutoScalingGroupName'])
-                    delete_autoscaling_group(
-                        autoscaling,
-                        autoscaling_group['AutoScalingGroupName']
-                        )
-
-    if block_s3 == 'true':
-        s3_buckets = get_s3_buckets(s3)
-        if s3_buckets:
-            for bucket in s3_buckets:
-                print(bucket['Name'])
-                block_s3_public_access(s3, bucket['Name'])
-
-    if disable_iam_users == 'true':
-        iam_users = get_iam_users(iam)
-        if iam_users:
-            for iam_user in iam_users[0]:
-                print(iam_user['UserName'])
-                delete_iam_user_profile(iam, iam_user['UserName'])
-                disable_iam_user_access_keys(
-                    iam_resource,
-                    iam_user['UserName']
-                    )
-    return
+    logger.info("Processing S3")
+    process_s3()
+    process_iam()
 
 
 if __name__ == "__main__":
-    event = ''
-    context = ''
-    main(event, context)
+    EVENT = ''
+    CONTEXT = ''
+    main(EVENT, CONTEXT)
